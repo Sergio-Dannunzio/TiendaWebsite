1facab307795f14908634a6237798dc16ab7508e
[Sergio-Dannunzio] <sergiodannunzio@hotmail.com>
2024-08-19 18:18:40 -0300

    fix ghpages

--- docs/server/main.server.mjs
+++ docs/server/main.server.mjs
@@ -1,2 +1,2 @@
 import './polyfills.server.mjs';
-import{a}from"./chunk-VM6WUSSA.mjs";import"./chunk-27Y7YGEC.mjs";import"./chunk-5XUXGTUW.mjs";export{a as default};
+import{a}from"./chunk-NZ42BRIG.mjs";import"./chunk-27Y7YGEC.mjs";import"./chunk-5XUXGTUW.mjs";export{a as default};
