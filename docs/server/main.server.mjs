4b3791bf66120c6976a4bf008d217fd0800368d1
[Sergio-Dannunzio] <sergiodannunzio@hotmail.com>
2024-08-31 09:39:00 -0300

    deploy

--- docs/server/main.server.mjs
+++ docs/server/main.server.mjs
@@ -1,2 +1,2 @@
 import './polyfills.server.mjs';
-import{a}from"./chunk-XYP65AEG.mjs";import"./chunk-27Y7YGEC.mjs";import"./chunk-5XUXGTUW.mjs";export{a as default};
+import{a}from"./chunk-V7NUCYNX.mjs";import"./chunk-27Y7YGEC.mjs";import"./chunk-5XUXGTUW.mjs";export{a as default};
