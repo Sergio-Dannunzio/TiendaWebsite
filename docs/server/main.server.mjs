dc4fbf83d793440d0dd70178dfbd368c2b1b3a83
[Sergio-Dannunzio] <sergiodannunzio@hotmail.com>
2024-09-05 09:57:58 -0300

    fix ghpages

--- docs/server/main.server.mjs
+++ docs/server/main.server.mjs
@@ -1,2 +1,2 @@
 import './polyfills.server.mjs';
-import{a}from"./chunk-YKGKQLX6.mjs";import"./chunk-TEP7RRHR.mjs";import"./chunk-5XUXGTUW.mjs";export{a as default};
+import{a}from"./chunk-QFGR2WMI.mjs";import"./chunk-TEP7RRHR.mjs";import"./chunk-5XUXGTUW.mjs";export{a as default};
